81754b4c7a648f9de7838fe506c18ea7e6afa1f5
Tijs van der Storm <storm@cwi.nl>
2014-04-22 23:08:59 +0200

    Removed main.

--- QL/src/ql_obj_alg/parser/TheParser.java
+++ QL/src/ql_obj_alg/parser/TheParser.java
@@ -3,25 +3,14 @@ package ql_obj_alg.parser;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
+
+import noa.Builder;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import ql_obj_alg.box.IFormat;
-import ql_obj_alg.operation.printer.ExprFormat;
-import ql_obj_alg.operation.printer.ExprPrecedence;
-import ql_obj_alg.operation.printer.FormFormat;
-import ql_obj_alg.operation.printer.StmtFormat;
-import ql_obj_alg.operation.typechecker.ExprTypeChecker;
-import ql_obj_alg.operation.typechecker.FormTypeChecker;
-import ql_obj_alg.operation.typechecker.ITypeCheck;
-import ql_obj_alg.operation.typechecker.StmtTypeChecker;
-import ql_obj_alg.report_system.error_reporting.ErrorReporting;
 import ql_obj_alg.syntax.IAllAlg;
-import ql_obj_alg.types.TypeEnvironment;
-import noa.Builder;
 
 public class TheParser {
 	
@@ -45,19 +34,6 @@ public class TheParser {
 	    qlParser.setErrorHandler(new BailErrorStrategy());
 	    qlParser.removeErrorListeners();
 		return (Builder) qlParser.f()._f;
-	}
- 	
-	
-	public static void main(String[] args) throws IOException {
-		Builder b = parse("form x { x: \"bla\" integer = (3) }");
-		IFormat formatter = b.build(new FormFormat(), new StmtFormat(), new ExprFormat<>(new ExprPrecedence()));
-		ITypeCheck checker = b.build(new FormTypeChecker(), new StmtTypeChecker(), new ExprTypeChecker());
-		ErrorReporting errs = new ErrorReporting();
-		checker.check(new TypeEnvironment(), errs);
-		errs.printErrors();
-		StringWriter w = new StringWriter();
-		formatter.format(0, true, w);
-		System.out.println(w.toString());
 	}
 
 }
