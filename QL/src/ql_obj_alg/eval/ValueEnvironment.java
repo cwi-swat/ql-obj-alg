23534bb0fc13880a9b9d00443fd78b685489dda0
Tijs van der Storm <storm@cwi.nl>
2014-04-23 00:03:59 +0200

    Forgot this one.

--- QL/src/ql_obj_alg/eval/ValueEnvironment.java
+++ QL/src/ql_obj_alg/eval/ValueEnvironment.java
@@ -4,16 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import ql_obj_alg.eval.values.Value;
-import ql_obj_alg.render.widgets.ObservableWidget;
 
 public class ValueEnvironment {
 
 	private Map<String,Value> questions;
-	private Map<String, ObservableWidget> registry;
 	
 	public ValueEnvironment(){
 		questions = new HashMap<String,Value>();
-		registry = new HashMap<String,ObservableWidget>();
 	}
 	
 	public Value getQuestionValue(String id){
