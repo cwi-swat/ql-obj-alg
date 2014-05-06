4cb0404b9b3947bd22022dd49f8246af8f9bcc1e
Mary Gouseti <mgouseti@gmail.com>
2014-05-06 14:41:39 +0200

    File separator

--- QLWithModulo/src/ql_obj_alg_extended/parse/PGenWithModulo.java
+++ QLWithModulo/src/ql_obj_alg_extended/parse/PGenWithModulo.java
@@ -1,11 +1,13 @@
 package ql_obj_alg_extended.parse;
 
+import java.io.File;
+
 import ql_obj_alg.syntax.Tokens;
 import ql_obj_alg_extended.syntax.IAllAlgWithModulo;
 
 public class PGenWithModulo {
 	public static void main(String[] args) {
 		noa.PGen pgen = new noa.PGen(Tokens.class, IAllAlgWithModulo.class);
-		pgen.generate("QLWithModulo", "ql_obj_alg_extended.parse", "src/ql_obj_alg_extended/parse/", true);
+		pgen.generate("QLWithModulo", "ql_obj_alg_extended.parse", "src"+File.pathSeparator+"ql_obj_alg_extended"+File.pathSeparator+"parse"+File.pathSeparator, true);
 	}
 }
