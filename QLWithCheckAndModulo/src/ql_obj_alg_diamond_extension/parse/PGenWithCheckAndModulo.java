ee0e482eeab0e1834dd3b5421a2a20a5b3a31c90
Mary Gouseti <mgouseti@gmail.com>
2014-05-14 10:56:39 +0200

    Updated destination of antlr generated files

--- QLWithCheckAndModulo/src/ql_obj_alg_diamond_extension/parse/PGenWithCheckAndModulo.java
+++ QLWithCheckAndModulo/src/ql_obj_alg_diamond_extension/parse/PGenWithCheckAndModulo.java
@@ -6,6 +6,6 @@ import ql_obj_alg_extended.syntax.TokensWithProperty;
 public class PGenWithCheckAndModulo {
 	public static void main(String[] args) {
 		noa.PGen pgen = new noa.PGen(TokensWithProperty.class, IAllAlgWithCheckAndModulo.class);
-		pgen.generate("QLWithCheckAndModulo", "ql_obj_alg_double_extention.parse", "src/ql_obj_alg_double_extention/parse/", true);
+		pgen.generate("QLWithCheckAndModulo", "ql_obj_alg_diamond_extension.parse", "src/ql_obj_alg_diamond_extension/parse/", true);
 	}
 }
